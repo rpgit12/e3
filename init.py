5d42d00d55d1e07e0d1dc5a2ba2031b3090a45e0
roman <rputtkammer@gmail.com>
2016-05-12 22:54:41 +0000

    changes from Thu May 12 22:54:41 UTC 2016

---
+++ init.py
@@ -0,0 +1,8 @@
+from ev3dev.auto import OUTPUT_A, Motor 
+from ev3dev.auto import OUTPUT_B, Motor 
+from ev3dev.auto import OUTPUT_C, Motor 
+import time
+
+motor_steer = Motor(OUTPUT_A)
+motor_left = Motor(OUTPUT_B)
+motor_right = Motor(OUTPUT_C)
